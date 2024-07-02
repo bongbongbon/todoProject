891a35f176ec9b6bde63bb75aca005f2610e4796
bongbong2 <106155992+bongbongbon@users.noreply.github.com>
2024-07-03 00:40:03 +0900

    CICD

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
-# Base image
-FROM openjdk:17-alpine
+# OpenJDK 17을 기반으로 하는 Docker 이미지 사용
+FROM adoptopenjdk/openjdk17:jre-17.0.1_12-alpine
 
-# Build arguments
-ARG JAR_FILE=build/libs/*.jar
-ARG PROFILES
-ARG ENV
+# Docker 컨테이너 내부의 /tmp 디렉토리에 볼륨 마운트
+VOLUME /tmp
 
-# Copy JAR file into the container
+# 빌드된 JAR 파일을 Docker 이미지 내부로 복사
+ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
 
-# Set the entry point
-ENTRYPOINT ["java", "-Dspring.profiles.active=${PROFILES}", "-Dserver.env=${ENV}", "-jar", "app.jar"]
+# Docker 컨테이너 실행 시 실행될 명령어 지정
+ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/example/cicdtest/controller/HealthCheckController.java
+++ src/main/java/com/example/cicdtest/controller/HealthCheckController.java
@@ -11,31 +11,31 @@ import java.util.TreeMap;
 
 @RestController
 public class HealthCheckController {
-
-    @Value("${server.env}")
-    private String env;
-    @Value("${server.port}")
-    private String serverPort;
-    @Value("${server.serverAddress}")
-    private String serverAddress;
-    @Value("${serverName}")
-    private String serverName;
-
-    @GetMapping("/hc")
-    public ResponseEntity<?> healthCheck() {
-        Map<String, String> responseData = new TreeMap<>();
-
-        responseData.put("serverName", serverName);
-        responseData.put("serverAddress", serverAddress);
-        responseData.put("serverPort", serverPort);
-        responseData.put("env", env);
-
-        return ResponseEntity.ok(responseData);
-    }
-
-    @GetMapping("/env")
-    public ResponseEntity<?> getEnv() {
-
-        return ResponseEntity.ok(env);
-    }
+//
+//    @Value("${server.env}")
+//    private String env;
+//    @Value("${server.port}")
+//    private String serverPort;
+//    @Value("${server.serverAddress}")
+//    private String serverAddress;
+//    @Value("${serverName}")
+//    private String serverName;
+//
+//    @GetMapping("/hc")
+//    public ResponseEntity<?> healthCheck() {
+//        Map<String, String> responseData = new TreeMap<>();
+//
+//        responseData.put("serverName", serverName);
+//        responseData.put("serverAddress", serverAddress);
+//        responseData.put("serverPort", serverPort);
+//        responseData.put("env", env);
+//
+//        return ResponseEntity.ok(responseData);
+//    }
+//
+//    @GetMapping("/env")
+//    public ResponseEntity<?> getEnv() {
+//
+//        return ResponseEntity.ok(env);
+//    }
 }
