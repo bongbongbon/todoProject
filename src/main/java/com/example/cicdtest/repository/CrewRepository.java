9866a9f145c4a3928c7a650dcde7d550e09c5887
bongbong2 <106155992+bongbongbon@users.noreply.github.com>
2024-07-03 04:26:38 +0900

    1

--- src/main/java/com/example/cicdtest/repository/CrewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cicdtest.repository;
-
-import com.example.cicdtest.domain.crew.Crew;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CrewRepository extends JpaRepository<Crew, Long> {
-}
